a4b9979e1c5f1f5bebefd920a96e36642523980a
Tomasz Romik <manetamajster@gmail.com>
2017-03-03 19:56:56 +0100

    Add contexts to rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "GET #index" do
-    it "should redirect to login page" do
-      get :index
-      expect(response).to redirect_to controller: 'devise/sessions', action: 'new'
+    context "user signed out" do
+      it "should redirect to login page" do
+        get :index
+        expect(response).to redirect_to controller: 'devise/sessions', action: 'new'
+      end
     end
 
-    it "should show users listing" do
-      jon = User.create email: 'jon@example.com', password: 'test'
-      jane = User.create email: 'jane@example.com', password: 'test'
-      sign_in jon
-      get :index
-      expect(assigns :users).to eq [jon, jane]
-      expect(response).to render_template :index
+    context "user signed in" do
+      before(:each) do
+        @jon = User.create email: 'jon@example.com', password: 'test'
+        @jane = User.create email: 'jane@example.com', password: 'test'
+      end
+
+      it "should show users listing" do
+        sign_in @jon
+        get :index
+        expect(assigns :users).to eq [@jon, @jane]
+        expect(response).to render_template :index
+      end
     end
   end
 end
