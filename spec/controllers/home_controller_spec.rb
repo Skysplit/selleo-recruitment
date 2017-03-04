ad7ecd6dd1b9ed9a15a19931887c5435da4bb7ff
Tomasz Romik <manetamajster@gmail.com>
2017-03-04 18:55:14 +0100

    make context names more verbose

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
-    context "user signed out" do
+    context "when user is signed out" do
       it "should redirect to login page" do
         get :index
         expect(response).to redirect_to controller: 'devise/sessions', action: 'new'
       end
     end
 
-    context "user signed in" do
+    context "when user is signed in" do
       it "should redirect to users page" do
         user = User.create email: 'test@example.com', password: 'test'
         sign_in user
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "GET #index" do
-    context "user signed out" do
+    context "when user is signed out" do
       it "should redirect to login page" do
         get :index
         expect(response).to redirect_to controller: 'devise/sessions', action: 'new'
       end
     end
 
-    context "user signed in" do
+    context "when user is signed in" do
       before(:each) do
         @jon = User.create email: 'jon@example.com', password: 'test'
         @jane = User.create email: 'jane@example.com', password: 'test'
