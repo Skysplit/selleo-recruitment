79e79c0317d79990ae0d62ad24052922589968e3
Tomek Romik <manetamajster@gmail.com>
2017-03-09 14:13:01 +0000

    Simplify csv helper spec expectations

--- spec/helpers/csv_helper_spec.rb
+++ spec/helpers/csv_helper_spec.rb
@@ -3,25 +3,22 @@ require "rails_helper"
 RSpec.describe CsvHelper, type: :helper do
   describe "#create_csv_string" do
     it "should return csv headers" do
-      headers = ['tets2', 'test2']
+      headers = ['test2', 'test2']
 
-      expect(CsvHelper.create_csv_string(headers: headers, values: [])).to eq(headers.map(&:humanize).join(',') + "\n")
+      expect(CsvHelper.create_csv_string(headers: headers, values: [])).to eq "Test2,Test2\n"
     end
 
     it "should return csv values" do
       values = [["a", "b", "c"]]
 
-      expect(CsvHelper.create_csv_string(values: values)).to eq (values.first.join(',') + "\n")
+      expect(CsvHelper.create_csv_string(values: values)).to eq "a,b,c\n"
     end
     
     it "should return csv with headers and values" do
       headers = ['test', 'test']
       values = [['lorem', 'ipsum']]
       
-      expected = headers.map(&:humanize).join(',') + "\n"
-      expected += values.first.join(',') + "\n"
-      
-      expect(CsvHelper.create_csv_string(values: values, headers: headers)).to eq expected
+      expect(CsvHelper.create_csv_string(values: values, headers: headers)).to eq "Test,Test\nlorem,ipsum\n"
     end
   end
 end
